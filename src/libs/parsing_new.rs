39b1d30e703619e328693458b82d92168196a9d9
Maximillian Weber <maxdweber7@gmail.com>
2024-04-29 18:44:58 -0700

    Re-implement shared config

--- src/libs/parsing_new.rs
+++ src/libs/parsing_new.rs
@@ -7,7 +7,7 @@ pub mod json_parser {
     use relative_path::RelativePath;
     use shared_config::ShareableConfiguration;
     use std::env::current_dir;
-    use std::path::Path;
+    use std::path::{Path, PathBuf};
     use std::{collections::HashMap, fs};
     use std::fmt::Error;
     /// Parses a map of JSON information into a HashMap<String,String>
@@ -27,7 +27,8 @@ pub mod json_parser {
         }
         map
     }
-
+    /// Take an input file name and parse the file to a JsonValue
+    /// 
     pub fn file_to_json(filename: &str) -> JsonValue {
         let file_contents = fs::read_to_string(filename).unwrap_or_else(|err| {
             output_error(CustomError::PARSINGERROR(Box::new(err)));
@@ -39,14 +40,107 @@ pub mod json_parser {
         });
         return parsed_data;
     }
+
+    fn parse_shared_config(json: &JsonValue) -> ShareableConfiguration {
+        let root = &current_dir().unwrap();
+        // let backend = check_backend(json);
+        ShareableConfiguration::new(
+            get_mapped_property(json, "metadata"),
+            get_property(json, "title"),
+            get_mapped_property(json, "tags"),
+            get_property(json, "language").unwrap(),
+            get_property(json, "image"),
+            get_property(json, "backend").unwrap(),
+            get_path_as_string(get_property(json, "output_directory"), root, true),
+            get_path_as_string(get_property(json, "source_directory"), root, false),
+        )
+    }
+
+    fn get_property(json: &JsonValue, property: &str) -> Option<String> {
+        if json[property].is_null() {
+            match property {
+                "backend" => {
+                    Some("bash".to_string())
+                },
+                "language" => {
+                    Some("Python".to_string())
+                },
+                _ => None
+            }
+        } else {
+            match property {
+                _ => Some(json[property].to_string())
+
+            }
+        }
+    }
+
+    fn get_mapped_property(json: &JsonValue, property: &str) -> Option<HashMap<String, String>> {
+        if json[property].is_null() {
+            None
+        } else {
+            Some(parse_json_map(&json[property]))
+        }
+    }
+
+    fn get_path_as_string(path: Option<String>, root: &PathBuf, is_output: bool) -> String {
+        if is_output {
+            match path {
+                Some(path) => {
+                    RelativePath::new(&path)
+                        .to_path(root)
+                        .to_str()
+                        .unwrap()
+                        .to_string()
+                }
+                None => {
+                    RelativePath::new("./dist/cider")
+                        .to_path(root)
+                        .to_str()
+                        .unwrap()
+                        .to_string()
+                }
+            }
+        } else {
+            match path {
+                Some(path) => {
+                    if path.starts_with('/') || path.contains(":") {
+                        Path::new(&path)
+                            .to_owned()
+                            .to_str()
+                            .unwrap()
+                            .to_owned()
+                    } else {
+                        RelativePath::new(&path)
+                            .to_path(&root)
+                            .to_str()
+                            .unwrap()
+                            .to_string()
+                    }
+                }
+                None => {
+                    RelativePath::new("./")
+                        .to_path(&root)
+                        .to_str()
+                        .unwrap()
+                        .to_string()
+                }
+            }
+        }
+    }
+
+
     #[cfg(test)]
     mod tests {
-        use super::*;
+        use crate::libs::parsing_new::json_parser::*;
         #[test]
         #[should_panic]
         fn test_json_parse_failure() {
-            let result =file_to_json("Pathtofilethatdoesnnotexist");
+            let result = file_to_json("Pathtofilethatdoesnnotexist");
         }
     }
 }
+
+
+
 
